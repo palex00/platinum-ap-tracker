9481b807583628a284d3e56ae9c183669beea380
palex00 <palex000000@gmail.com>
2026-02-10 22:05:01 +0100

    attempt to fix sunyshore logic

--- scripts/logic/logic.lua
+++ scripts/logic/logic.lua
@@ -110,7 +110,7 @@ function early_sunyshore()
     if has("opt_early_sunyshore_on") then
         return AccessibilityLevel.Normal
     else
-        return math.max(has_level("event_clear_distortion") and Tracker:FindObjectForCode("@event_clear_distortion").AccessibilityLevel)
+        return has_level("event_clear_distortion") and Tracker:FindObjectForCode("@event_clear_distortion").AccessibilityLevel
     end
 end
 
