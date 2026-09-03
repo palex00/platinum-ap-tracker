d6cc030733ce15fc773b8ceeccb9d03de7030564
palex00 <palex000000@gmail.com>
2026-09-03 22:19:45 +0200

    remove dead code

--- scripts/watches.lua
+++ scripts/watches.lua
@@ -58,7 +58,6 @@ end
 local hosted_specific = {"coupon_1", "coupon_2", "coupon_3", "pokedex_1", "pokedex_2", "pokedex_3"}
 for _, code in ipairs(UNOWN_ITEMS) do table.insert(hosted_specific, code) end
 for _, code in ipairs(hosted_specific) do
-    ScriptHost:AddWatchForCode(code.."_hostsync", code, syncHostedFromBase)
     ScriptHost:AddWatchForCode(code.."_hosted", code.."_hosted", syncBaseFromHosted)
 end
 
